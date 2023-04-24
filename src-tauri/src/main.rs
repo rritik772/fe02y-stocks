1a865b2842c76ddd27a1701d0955117a2c550e47
ritik ranjan <rritik772@outlook.com>
2023-04-24 09:49:08 +0530

    added `tauri::command`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod nse_data;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +11,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            nse_data::get_index_data,
+            nse_data::get_stock_data,
+            nse_data::get_all_stocks,
+            nse_data::get_top_gainers,
+            nse_data::get_top_losers,
+            nse_data::get_top_volumes
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/nse_data.rs
@@ -0,0 +1,35 @@
+use stock_screener_lib::{
+    indexes::{index_response::IndexResponse, indexcode::IndexCode, nse},
+    nsestocks::{stock, allstocks},
+    performance::{top_gainers, top_losers, top_volume, performance_response}
+};
+
+#[tauri::command]
+pub async fn get_index_data(api_key: String, indexes: Vec<IndexCode>) -> Result<IndexResponse, u16> {
+    return nse::get_index_data(api_key, indexes).await;
+}
+
+#[tauri::command]
+pub async fn get_stock_data(api_key: String, stocks: Vec<String>) -> Result<stock::Stock, u16> {
+    stock::get_stock_data(api_key, stocks).await
+}
+
+#[tauri::command]
+pub async fn get_all_stocks(api_key: String) -> Result<allstocks::AllStockResponse, u16> {
+    allstocks::get_all_stocks(api_key).await
+}
+
+#[tauri::command]
+pub async fn get_top_gainers(api_key: String) -> Result<performance_response::PerformanceResponse, u16> {
+    top_gainers::get_top_gainers(api_key).await
+}
+
+#[tauri::command]
+pub async fn get_top_losers(api_key: String) -> Result<performance_response::PerformanceResponse, u16> {
+    top_losers::get_top_losers(api_key).await
+}
+
+#[tauri::command]
+pub async fn get_top_volumes(api_key: String) -> Result<performance_response::PerformanceResponse, u16> {
+    top_volume::get_top_volumes(api_key).await
+}
